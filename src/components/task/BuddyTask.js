45deb0bd86046a8b1aef54caee0adf88438827a7
denalse <denaleewonders@gmail.com>
2024-07-18 22:00:55 +0800

    eighth commit: update buddy task page

--- src/components/task/BuddyTask.js
+++ src/components/task/BuddyTask.js
@@ -1,26 +1,42 @@
 import React from "react";
-import { useState } from "react";
-import { AddTask } from "./AddTask";
-import { ShowTask } from "./ShowTask";
+// import { useState } from "react";
+// import { AddTask } from "./AddTask";
+// import { ShowTask } from "./ShowTask";
 import "../../style/BuddyTask.css";
 
 export const BuddyTask = () => {
-  const [tasklist, setTasklist] = useState([]);
-  const [task, setTask] = useState({});
+  const users = [
+    { id: 1, name: "Janice Rolland", tasks: ['12:00pm: Eat Lunch', '1:00pm: Eat Medication']  },
+    { id: 2, name: "Ken Junior", tasks: ['10:00am: Go to Work', '10:30am: Scrum Meeting'] },
+  ];
+
   return (
-    <div class="task-container">
-      <AddTask
-        tasklist={tasklist}
-        setTasklist={setTasklist}
-        task={task}
-        setTask={setTask}
-      />
-      <ShowTask
-        tasklist={tasklist}
-        setTasklist={setTasklist}
-        task={task}
-        setTask={setTask}
-      />
+    <div className="buddy-page-container">
+      <div className="user-list-container">
+        <h2>Users You Are Buddy With</h2>
+        <ul className="user-list">
+          {users.map((user) => (
+            <li key={user.id} className="user-item">
+              <h3>{user.name}</h3>
+              <ul className="task-list">
+                {user.tasks.map((task, index) => (
+                  <li key={index} className="task-item">{task}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="add-task-container">
+        <h2>Add Task</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="task">Task</label>
+            <input type="text" id="task" name="task" required />
+          </div>
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/task/UserTask.js
+++ src/components/task/UserTask.js
@@ -1,26 +1,35 @@
 import React from "react";
-import { useState } from "react";
-import { AddTask } from "./AddTask";
-import { ShowTask } from "./ShowTask";
 import "../../style/UserTask.css";
 
 export const UserTask = () => {
-  const [tasklist, setTasklist] = useState([]);
-  const [task, setTask] = useState({});
+  var tasks = [
+    { id: 1, name: "Janice Rolland" },
+    { id: 2, name: "Ken Junior" },
+    { id: 3, name: "Mandy Brooklyn" },
+  ];
+
   return (
-    <div class="task-container">
-      <AddTask
-        tasklist={tasklist}
-        setTasklist={setTasklist}
-        task={task}
-        setTask={setTask}
-      />
-      <ShowTask
-        tasklist={tasklist}
-        setTasklist={setTasklist}
-        task={task}
-        setTask={setTask}
-      />
+    <div className="user-page-container">
+      <div className="user-list-container">
+        <h2>My Schedule</h2>
+        <ul className="user-list">
+          {tasks.map((task) => (
+            <li key={task.id} className="user-item">
+              {task.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="add-task-container">
+        <h2>Add Task</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="task">Task</label>
+            <input type="text" id="task" name="task" required />
+          </div>
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
     </div>
   );
 };
